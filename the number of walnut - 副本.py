41c47e0cdc558dcc396c332031ab82a6d087fb9c
Shyuna <1229324428@qq.com>
2020-10-28 12:14:08 +0800

    test push

---
+++ the number of walnut - 副本.py
@@ -0,0 +1,17 @@
+stra,strb,strc=input().strip().split()
+a=int(stra)
+b=int(strb)
+c=int(strc)
+for i in range(1,27000):
+    if i%a==0:
+        if i%b==0:
+            if i%c==0:
+                print(i)
+                break;
+            else:
+                continue;
+        else:
+            continue;
+    else:
+        continue;
+#test change
